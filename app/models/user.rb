62c6aae2971532050f805882314756f8e7e30971
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-25 22:24:08 +0100

    - fix user model spec failing tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# User model
 class User < ApplicationRecord
+  has_many :properties
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
 end
