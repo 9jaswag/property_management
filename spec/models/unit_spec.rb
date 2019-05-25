584862da59bbf2bd4a2bc5ca8e93d9e1519e0c5c
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-25 20:53:20 +0100

    - add validation specs for unit model

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Unit, type: :model do
   describe 'database index' do
     it { is_expected.to have_db_index(:type) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:type) }
+  end
 end
