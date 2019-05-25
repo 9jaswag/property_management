2c0bd22f1c6f89b57ddd453e5c21cd8fa25dd98a
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-25 16:27:22 +0100

    - fix address model failing validation specs

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# Address model
 class Address < ApplicationRecord
+  validates :street, presence: true
+  validates :zipcode, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
 end
