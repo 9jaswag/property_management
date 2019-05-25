72256eb2dbdeecee431f6275c9482a0dd39dc8ba
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-25 16:16:18 +0100

    - add address model validation specs

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:street) }
+    it { is_expected.to validate_presence_of(:zipcode) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:state) }
+    it { is_expected.to validate_presence_of(:country) }
+  end
 end
