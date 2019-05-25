ec5ce17b9db162c6d6db9bf04757bb63287645c2
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-25 16:28:51 +0100

    - add address model full_address instance method & database index spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Address, type: :model do
     it { is_expected.to validate_presence_of(:state) }
     it { is_expected.to validate_presence_of(:country) }
   end
+
+  describe 'database index' do
+    it { is_expected.to have_db_index(:street) }
+    it { is_expected.to have_db_index(:zipcode) }
+    it { is_expected.to have_db_index(:city) }
+    it { is_expected.to have_db_index(:state) }
+    it { is_expected.to have_db_index(:country) }
+  end
+
+  describe '#full_address' do
+    it 'returns a property\'s full address' do
+      address = build(:address)
+
+      expect(address.full_address)
+        .to eq '235 Ikorodu Rd, 100252, Illupeju, Lagos, Nigeria'
+    end
+  end
 end
