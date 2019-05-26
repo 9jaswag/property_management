475e67932506256d7bfcb388f81a7ca6101d5149
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-26 13:53:32 +0100

    - add user model email uniqueness validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:password_digest) }
     it { is_expected.to validate_presence_of(:email) }
+
+    subject { User.new(name: 'John Doe', email: 'JOHNDOE@email.com') }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   end
 
   describe 'associations' do
