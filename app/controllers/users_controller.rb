d4af8c4f6cf7d971a0e9056bb2e217604e4e7696
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-26 14:30:47 +0100

    - fix failing user controller specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
+# frozen_string_literal: true
+
+# Users controller
 class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+
+  def create
+    user = User.create!(user_params)
+    token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: 'Account created successfully', token: token }
+
+    render json: response, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
 end
