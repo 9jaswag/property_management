af701178efce24f75d85a9f054c13eb721a4d71a
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-27 14:13:41 +0100

    chore: write validation specs for tenant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_190008) do
+ActiveRecord::Schema.define(version: 2019_05_27_130753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2019_05_26_190008) do
     t.index ["address_id"], name: "index_properties_on_address_id"
     t.index ["unit_id"], name: "index_properties_on_unit_id"
     t.index ["user_id"], name: "index_properties_on_user_id"
+  end
+
+  create_table "tenants", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "units", force: :cascade do |t|
