413e5990300aee729819c1892ce4ac7e3d19c7a9
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-27 14:29:39 +0100

    chore: add tenants controller specs

--- spec/controllers/v1/tenants_controller_spec.rb
+++ spec/controllers/v1/tenants_controller_spec.rb
@@ -1,5 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe V1::TenantsController, type: :controller do
+RSpec.describe V1::TenantsController, type: :request do
+  let(:user) { create(:user) }
+  let(:headers) { valid_headers }
+  let(:valid_attributes) { attributes_for(:tenant) }
 
+  describe 'POST /tenants' do
+    context 'when request is valid' do
+      before { post '/tenants', params: valid_attributes.to_json, headers: headers }
+
+      it 'creates a new tenant' do
+        expect(response).to have_http_status(201)
+        expect(json_body['name']).to eq(valid_attributes[:name])
+      end
+    end
+  end
 end
