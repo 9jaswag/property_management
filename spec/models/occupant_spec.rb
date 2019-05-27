a0a48959fb84a58316aab18ed948091c8fcb9ae4
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-05-27 15:17:27 +0100

    - add occupant model validation

--- spec/models/occupant_spec.rb
+++ spec/models/occupant_spec.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Occupant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:property_id) }
+    it { should validate_presence_of(:tenant_id) }
+  end
 end
